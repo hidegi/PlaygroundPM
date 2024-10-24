61758b19a1d4684200df35bca42c423e8d5ad626
dane <nachv2010@gmail.com>
2024-10-24 10:20:28 +0200

    initial commit

--- include/ecs/sprite_component.h
+++
@@ -1,9 +0,0 @@
-#ifndef SPRITECOMPONENT_H_INCLUDED
-#define SPRITECOMPONENT_H_INCLUDED
-#include <sp/gxsp/sprite.h>
-struct SpriteComponent
-{
-    std::shared_ptr<sp::Sprite>   sprite;
-};
-
-#endif // SPRITECOMPONENT_H_INCLUDED
